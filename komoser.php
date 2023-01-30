0301f530c5dca7e6c8bf26a4ed8ebc336e9c783b
Your Name <you@example.com>
2023-01-30 07:05:51 -0800

    new

---
+++ komoser.php
@@ -0,0 +1,4 @@
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
